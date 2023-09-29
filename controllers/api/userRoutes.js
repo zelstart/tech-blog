91546123badc402ac01c776ec64d31b3547d1f01
zelstart <e.pomeroy95@gmail.com>
2023-09-29 18:40:22 -0400

    added routes for posts and users

---
+++ controllers/api/userRoutes.js
@@ -0,0 +1,77 @@
+const router = require('express').Router();
+const { User, Post } = require('../../models');
+
+
+// sign up
+router.post('/', async (req, res) => {
+    console.log('POST route hit!');
+    try {
+        const newUser = await User.create({
+            name: req.body.name,
+            password: req.body.password
+        });
+        
+        console.log(req.body.name, req.body.password);
+
+        req.session.save(() => {
+            req.session.userId = newUser.id; 
+            req.session.name = newUser.name;
+            // immediately log the user in on account creation
+            req.session.loggedIn = true;
+        });
+
+        res.json(newUser);
+        
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+
+// login
+router.post('/login', async (req, res) => {
+    try {
+        const user = await User.findOne({
+            where: {
+                name: req.body.name,
+            },
+        });
+        // check validity of user + password combo
+        if (!user || !user.checkPassword(req.body.password)) {
+            return res.status(400).json({ message: 'Invalid login. Please try again.' });
+        }
+
+        req.session.save(() => {
+            req.session.userId = user.id;
+            req.session.name = user.name;
+            req.session.loggedIn = true;
+
+            res.json({ user, message: `Welcome back, ${user.name}` }); 
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+// get all users 
+router.get('/', async (req, res) => {
+    try {
+        const users = await User.findAll();
+        res.json(users);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// get user by ID. include their posts
+router.get('/:id', async (req, res) => {
+    try {
+        const user = await User.findByPk(req.params.id, {
+            include: [{ model: Post }]
+        });
+        res.json(user);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
