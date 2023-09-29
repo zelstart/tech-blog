799463f0ee3db91e439a2aa707eb10820f1bd418
zelstart <e.pomeroy95@gmail.com>
2023-09-29 18:39:13 -0400

    fixed the routes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-const apiRoutes = require('./controllers');
+const routes = require('./controllers');
 // const helpers = require('./utils/helpers');
 
 const sequelize = require('./config/connection');
@@ -34,7 +34,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/api', apiRoutes);
+app.use(routes)
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
