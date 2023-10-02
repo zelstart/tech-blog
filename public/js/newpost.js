ebb73c2983b41b3e769a9725ac594a9729e719ca
zelstart <e.pomeroy95@gmail.com>
2023-10-02 17:04:05 -0400

    newpost partial + js logic

---
+++ public/js/newpost.js
@@ -0,0 +1,24 @@
+const newPostFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const title = document.querySelector('#post-title').value.trim();
+    const content = document.querySelector('#post-content').value.trim();
+  
+    if (title && content) {
+      const response = await fetch('/api/posts', {
+        method: 'POST',
+        body: JSON.stringify({ title, content }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/');
+      } else {
+        alert('Failed to create post');
+      }
+    }
+  };
+  
+  document
+    .querySelector('.newpost-form')
+    .addEventListener('submit', newPostFormHandler);
