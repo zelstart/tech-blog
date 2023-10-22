bea4db387188265274a8807596aaa7f83c9dbf2c
zelstart <e.pomeroy95@gmail.com>
2023-10-22 14:24:43 -0400

    added new post functionality

--- public/js/newpost.js
+++ public/js/newpost.js
@@ -5,7 +5,7 @@ const newPostFormHandler = async (event) => {
     const content = document.querySelector('#post-content').value.trim();
   
     if (title && content) {
-      const response = await fetch('/api/posts', {
+      const response = await fetch('/posts', {
         method: 'POST',
         body: JSON.stringify({ title, content }),
         headers: { 'Content-Type': 'application/json' },
