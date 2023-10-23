bd33ea06db53f797cae19997a43cdb2762004451
zelstart <e.pomeroy95@gmail.com>
2023-10-23 16:53:08 -0400

    added modal to confirm delete post

--- public/js/deletepost.js
+++ public/js/deletepost.js
@@ -1,24 +1,53 @@
+// document.addEventListener('click', async (event) => {
+//     if (event.target.classList.contains('delete-post')) {
+//         const postId = event.target.dataset.id;
+
+//         try {
+//             const response = await fetch(`/deletepost/${postId}`, {
+//                 method: 'DELETE',
+//                 headers: {
+//                     'Content-Type': 'application/json',
+//                 },
+//             });
+
+//             if (response.ok) {
+//                 window.location.href = '/dashboard';
+//             } else {
+//                 const errorMessage = await response.text();
+//                 alert(`Error: ${errorMessage}`);
+//             }
+//         } catch (error) {
+//             console.error(error);
+//             alert('An error occurred while deleting the post');
+//         }
+//     }
+// });
+
+
 document.addEventListener('click', async (event) => {
     if (event.target.classList.contains('delete-post')) {
         const postId = event.target.dataset.id;
 
-        try {
-            const response = await fetch(`/deletepost/${postId}`, {
-                method: 'DELETE',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-            });
+        // Handle the confirmation modal button click
+        document.getElementById('confirmDelete').addEventListener('click', async () => {
+            try {
+                const response = await fetch(`/deletepost/${postId}`, {
+                    method: 'DELETE',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                });
 
-            if (response.ok) {
-                window.location.href = '/dashboard';
-            } else {
-                const errorMessage = await response.text();
-                alert(`Error: ${errorMessage}`);
+                if (response.ok) {
+                    window.location.href = '/dashboard'; // Redirect to the dashboard
+                } else {
+                    const errorMessage = await response.text();
+                    alert(`Error: ${errorMessage}`);
+                }
+            } catch (error) {
+                console.error(error);
+                alert('An error occurred while deleting the post');
             }
-        } catch (error) {
-            console.error(error);
-            alert('An error occurred while deleting the post');
-        }
+        });
     }
 });
